4a7e5327a315cbf638af68ffcd6986e7828611ba
vijaychari <103813226+vijaychari@users.noreply.github.com>
2022-04-18 16:17:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Welcome from './components/welcome';
 const App = () => {
   return (
     <div> 
-      <h1>Hello This is My first react APP </h1> 
+      <h1>hi chinna</h1> 
       <Welcome/>
       <button> Click Me</button> 
     </div>
