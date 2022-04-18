5e068e1f8e49db5dd95e43285ea69b4e846e0ef1
vijaychari <viji5738@gmail.com>
2022-04-18 15:08:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Welcome from './components/welcome';
+
+
+const App = () => {
+  return (
+    <div> 
+      <h1>Hello This is My first react APP </h1> 
+      <Welcome/>
+      <button> Click Me</button> 
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ src/components/welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <div> 
+        <h1> Guys please support me</h1>
+
+    </div>
+  )
+}
+export default Welcome;
